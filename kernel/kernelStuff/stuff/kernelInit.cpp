9f5adb35ef9260a9f65eacc5eded8eb5ad0723f4
Marceldobehere <marcel.z2006@gmail.com>
2023-08-30 03:38:24 +0200

    sdf

--- kernel/kernelStuff/stuff/kernelInit.cpp
+++ kernel/kernelStuff/stuff/kernelInit.cpp
@@ -157,6 +157,7 @@ void DoGdtStuff()
     GDTBlock* gdt_block = (GDTBlock*)GlobalAllocator->RequestPage();
     GlobalPageTableManager.MapMemory(gdt_block, gdt_block);
     char* stack_kernel = (char*)GlobalAllocator->RequestPages(8);
+    char* stack_kernel_end = stack_kernel + 0x1000 * 8;
     // GlobalPageTableManager.MapMemories(stack_kernel, stack_kernel, 8);
     // char* stack_isr = (char*)GlobalAllocator->RequestPages(8);
     // GlobalPageTableManager.MapMemories(stack_isr, stack_isr, 8);
@@ -166,7 +167,7 @@ void DoGdtStuff()
     // GlobalPageTableManager.MapMemories(stack_timer, stack_timer, 8);
 
     gdt_init(gdt_block);
-	gdt_set_tss_ring(gdt_block, 0, stack_kernel);
+	gdt_set_tss_ring(gdt_block, 0, stack_kernel_end);
 	// gdt_set_tss_ist(gdt_block, IDT_IST_ISR, stack_isr);
 	// gdt_set_tss_ist(gdt_block, IDT_IST_IRQ, stack_irq);
 	// gdt_set_tss_ist(gdt_block, IDT_IST_TIMER, stack_timer);
