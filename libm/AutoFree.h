9caed9ce48799cb2e54e669700e46d34ebeeda6c
SpaceFishDev <ultragamesalot@gmail.com>
2023-09-03 16:05:41 +0200

    fixed fr

--- libm/AutoFree.h
+++ libm/AutoFree.h
@@ -25,7 +25,9 @@ public:
     }
     AutoFree(T value);
     AutoFree(AutoFree<T> &other);
+    AutoFree(AutoFree<T> &other);
     AutoFree<T> operator=(AutoFree<T> &other);
+    AutoFree<T>::AutoFree(uint64_t size, T *value);
     T &operator*();
     T *operator->();
     ~AutoFree();
