47be55d290e7394449a3587900cd7660af9c4c76
Marceldobehere <marcel.z2006@gmail.com>
2023-11-06 23:44:30 +0100

    ok reverted some elf code lol

--- kernel/elf/elf.cpp
+++ kernel/elf/elf.cpp
@@ -55,18 +55,12 @@ namespace Elf
                         uint64_t* target = (uint64_t*) (elf_load_offset + rela->r_offset);
                         *target = elf_load_offset + rela->r_addend;
                     }
-                    else
-                    {
-                        Serial::Writelnf("ELF> Unknown relocation type: %x", rela->r_info);
-                        while (true);
-                    }
                 }
             }
         }
     }
 
     LoadedElfFile borkedElfFile = {NULL, 0, NULL, NULL, false};
-
 
     LoadedElfFile LoadElf(uint8_t* data)
     {
@@ -96,67 +90,43 @@ namespace Elf
         }
         
 
-        Elf64_Phdr ph;//(Elf64_Phdr*) (((char*) data) + header->e_phoff);
+        Elf64_Phdr* ph = (Elf64_Phdr*) (((char*) data) + header->e_phoff);
         //Serial::Writelnf("ELF> ph: %x\n", ph);
 
-        uint64_t base = NULL;
-        for (int i = 0; i < header->e_phnum; i++) 
+        void* last_dest = NULL;
+
+        for (int i = 0; i < header->e_phnum; i++, ph++) 
         {
-            _memcpy(data + header->e_phoff + header->e_phentsize*i, &ph, sizeof(ph));
-            base = min(base, ph.p_vaddr);
-        }
+            if (ph->p_type != PT_LOAD) 
+                continue;
     
-        uintptr_t size=0;
-    
-        for (int i = 0; i < header->e_phnum; i++) 
-        {
-            uintptr_t segment_end;
-            _memcpy(data + header->e_phoff + header->e_phentsize*i, &ph, sizeof(ph));
-    
-            segment_end = ph.p_vaddr - base + ph.p_memsz;
-            size = max(size, segment_end);
+            void* tempDest = (void*) ((uint64_t) ph->p_vaddr + ph->p_memsz);
+            if (last_dest == NULL || last_dest < tempDest)
+                last_dest = tempDest;
         }
 
-        // void* last_dest = NULL;
-
-        // for (int i = 0; i < header->e_phnum; i++, ph++) 
-        // {
-        //     _memcpy(data + header->e_phoff + header->e_phentsize*i, ph, sizeof(ph));
-        //     if (ph->p_type != PT_LOAD) 
-        //         continue;
-    
-        //     void* tempDest = (void*) ((uint64_t) ph->p_vaddr + ph->p_memsz);
-        //     //if (last_dest == NULL || last_dest < tempDest)
-        //     last_dest = tempDest;
-        // }
-
-        // void* offset = GlobalAllocator->RequestPages((uint64_t) last_dest / 0x1000 + 1);
-        // // TODO: might make a check here for kernel modules, so ya cant run or see it as userspace
-        // GlobalPageTableManager.MapMemories((void*)((uint64_t)offset + MEM_AREA_ELF_MAP_OFFSET), (void*)offset, (uint64_t) last_dest / 0x1000 + 1, PT_Flag_Present | PT_Flag_ReadWrite | PT_Flag_UserSuper);
-        // offset = (void*)((uint64_t)offset + MEM_AREA_ELF_MAP_OFFSET);
-        // //Serial::Writelnf("offset: %x\n", offset);
-        // _memset(offset, 0, (uint64_t) last_dest / 0x1000 + 1);
-
-        uint64_t pageCount = (uint64_t) size / 0x1000 + 5;
+        // + 10 bc we cool
+        uint64_t pageCount = (uint64_t) last_dest / 0x1000 + 10;
         void* offset = GlobalAllocator->RequestPages(pageCount);
+        // TODO: might make a check here for kernel modules, so ya cant run or see it as userspace
         GlobalPageTableManager.MapMemories((void*)((uint64_t)offset + MEM_AREA_ELF_MAP_OFFSET), (void*)offset, pageCount, PT_Flag_Present | PT_Flag_ReadWrite | PT_Flag_UserSuper);
         offset = (void*)((uint64_t)offset + MEM_AREA_ELF_MAP_OFFSET);
-        _memset(offset, 0, pageCount*0x1000);
+        _memset(offset, 0, pageCount * 0x1000);
 
-        //ph = (Elf64_Phdr*) (((char*) data) + header->e_phoff);
-        for (int i = 0; i < header->e_phnum; i++)
+        ph = (Elf64_Phdr*) (((char*) data) + header->e_phoff);
+        for (int i = 0; i < header->e_phnum; i++, ph++)
         {
-            _memcpy(data + header->e_phoff + header->e_phentsize*i, &ph, sizeof(ph));
-            void* dest = (void*) ((uint64_t) ph.p_vaddr - base + (uint64_t)offset);
-            void* src = ((char*) data) + ph.p_offset;
-    
-            if (ph.p_type != PT_LOAD)// && ph.p_type != PT_PHDR && ph.p_type != PT_DYNAMIC) 
+            void* dest = (void*) ((uint64_t) ph->p_vaddr + (uint64_t) offset);
+            void* src = ((char*) data) + ph->p_offset;
+
+
+            if (ph->p_type != PT_LOAD) 
                 continue;
+            
+            _memset(dest, 0, ph->p_memsz);
+            _memcpy(src, dest, ph->p_filesz);
 
-            _memset(dest, 0, ph.p_memsz);
-            _memcpy(src, dest, ph.p_filesz);
-
-            elf_apply_relocations(data, header, (uint64_t) ph.p_vaddr, (uint64_t) offset, ph.p_filesz);
+            elf_apply_relocations(data, header, (uint64_t) ph->p_vaddr, (uint64_t) offset, ph->p_filesz);
         }
         
 
@@ -164,7 +134,6 @@ namespace Elf
         file.entryPoint = (void*) (header->e_entry + (uint64_t) offset);
         file.offset = offset;
         file.data = data;
-        //file.size = (uint64_t) last_dest / 0x1000 + 1;
         file.size = pageCount;
         file.works = true;
 
