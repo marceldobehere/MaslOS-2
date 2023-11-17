ba0f1210d94ad08786bb9589b74311db18406c9f
Marceldobehere <marcel.z2006@gmail.com>
2023-11-17 11:23:24 +0100

    started work on porting maab interpreter
    will need a bit though, since i need to add stdio with this and window attributes, etc.

---
+++ programs/maab/maabTask.h
@@ -0,0 +1,20 @@
+// #pragma once
+// #include <libm/window/window.h>
+// #include <libm/gui/guiInstance.h>
+// #include "maabMem/maabMemHandler.h"
+// #include <stdint.h>
+// #include <stddef.h>
+
+// Window* FindWindowWithId(uint64_t winID);
+// GuiInstance* GetGuiFromWindow(Window* win);
+
+// void Cast(DatatypeNumber typeFrom, uint64_t addrFrom, DatatypeNumber typeTo, uint64_t addrTo);
+// void PrintVal(DatatypeNumber numType, uint64_t numAddr);
+// void ShowBytes(uint8_t amount, uint64_t addr);
+// void Math(OpNumber opNum, DatatypeNumber typeNum, uint64_t addr1, uint64_t addr2, uint64_t addrRes);
+
+
+// void Do();
+// void Free();
+// void PrintMem();
+
