ffb12f435084415f50519a31222fe839a51eff55
AptRock327 <dominik032009@gmail.com>
2023-12-25 18:47:28 +0100

    Added more mathematical functions

--- libm/math.cpp
+++ libm/math.cpp
@@ -14,6 +14,9 @@
 //     return b;
 // }
 
+const double MATH_PI = 3.14159265;
+const double MATH_EULER = 2.71828;
+
 MPoint::MPoint(int x, int y)
 {
     this->x = x;
@@ -58,4 +61,48 @@ double cos(double x)
 double tan(double x)
 {
     return sin(x)/cos(x);
+}
+
+double csc(double x)
+{
+    return 1/sin(x);
+}
+
+double sec(double x)
+{
+    return 1/cos(x);
+}
+
+double ctan(double x)
+{
+    return 1/tan(x);
+}
+
+double Asin(double x)
+{
+    return (double)(x + (pow(x, 3))/6 + (3 * pow(x, 5))/40 + (5 * pow(x, 7))/112 + (35 * pow(x, 9))/1152);
+}
+
+double Acos(double x)
+{
+    return (1.570796 - Asin(x));
+}
+
+double Atan(double x)
+{
+    return (double)(x - (pow(x, 3)/3) + (pow(x, 5)/5) - (pow(x, 7)/7) + (pow(x, 9)/9) - (pow(x,11)/11) + (pow(x,13)/13) - (pow(x,15)/15) + (pow(x,17)/17));
+}
+
+double approx_derivative(double (*f)(double), double point, double precision)
+{
+    double diff = (*f)(point) - (*f)(point+(0.01/precision));
+    return (diff * precision)/0.01;
+}
+
+double approx_integral(double (*f)(double), double a, double b, double precision)
+{
+    double length = (a-b)/(100*precision);
+    double RiemannSum = 0;
+    for(double i = a; i < b; i+=length) RiemannSum += length * (*f)(i);
+    return RiemannSum;
 }
--- libm/math.h
+++ libm/math.h
@@ -47,3 +47,11 @@ double pow(double x, int n);
 double sin(double x);
 double cos(double x);
 double tan(double x);
+double csc(double x);
+double sec(double x);
+double ctan(double x);
+double Asin(double x);
+double Acos(double x);
+double Atan(double x);
+double approx_derivative(double (*f)(double), double point, double precision);
+double approx_integral(double (*f)(double), double a, double b, double precision)
