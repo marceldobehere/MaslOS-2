5387b6b4f7ebaeb6d938acd08640cc12e6b08b73
Arcade Wise <l3gacy.b3ta@disroot.org>
2023-12-30 23:10:06 -0500

    make scripts more user friendly using bash -e

--- JUST RUN OS.sh
+++ JUST RUN OS.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 ./cDisk.sh
 qemu-system-x86_64 -machine q35 -m 1G -cpu qemu64 -serial stdio -soundhw ac97 -drive file=disk.img -boot d -cdrom MaslOS2.iso
 
--- cDisk.sh
+++ cDisk.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 if [ ! -f ./disk.img ]; then
     echo "<Creating disk.img>"
