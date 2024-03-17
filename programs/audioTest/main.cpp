c1b84767bd0c23f94efe64e8aecfe32bbc34d5ff
Marceldobehere <marcel.z2006@gmail.com>
2024-03-17 15:14:16 +0100

    maybe starting a bit of work on userspace audio system interface

---
+++ programs/audioTest/main.cpp
@@ -0,0 +1,64 @@
+#include <libm/syscallManager.h>
+#include <libm/syscallList.h>
+#include <libm/rendering/basicRenderer.h>
+#include <libm/rendering/Cols.h>
+
+#include <libm/cstr.h>
+#include <libm/cstrTools.h>
+#include <libm/wmStuff/wmStuff.h>
+#include <libm/rendering/basicRenderer.h>
+#include <libm/rendering/Cols.h>
+#include <libm/rnd/rnd.h>
+#include <libm/stubs.h>
+
+#include <libm/gui/guiInstance.h>
+#include <libm/gui/guiStuff/components/box/boxComponent.h>
+#include <libm/gui/guiStuff/components/rectangle/rectangleComponent.h>
+#include <libm/gui/guiStuff/components/text/textComponent.h>
+#include <libm/gui/guiStuff/components/button/buttonComponent.h>
+#include <libm/gui/guiStuff/components/textField/textFieldComponent.h>
+
+#include <libm/audio/internal/audio.h>
+
+int main(int argc, char** argv)
+{
+    // Global Audio Destination
+    Audio::BasicAudioDestination* globalAudioDest = NULL;
+
+    if (globalAudioDest == NULL)
+        return 0;
+
+
+    // Create our Audiosource
+    int sampleRate = 44100;
+    int sampleCount = sampleRate * 1;
+    Audio::BasicAudioSource* audioSource = new Audio::BasicAudioSource(
+        Audio::AudioBuffer::Create16BitMonoBuffer(sampleRate, sampleCount)
+    );
+
+    // Connect it to the Global Audio Destination
+    audioSource->ConnectTo(globalAudioDest);
+
+    // Fill the buffer with some audio
+    uint16_t* arr = (uint16_t*)audioSource->buffer->data;
+    int dif = 20;
+    for (int i = 0; i < dif; i++)
+        Audio::FillArray(arr, (i * sampleCount)/dif, sampleCount/dif, ((1000*(i+1)) / dif), sampleRate);
+    
+    // Set the sample count and reset the samples sent
+    audioSource->buffer->sampleCount = sampleCount;
+    audioSource->samplesSent = 0;
+
+    // Set the ready to send flag
+    audioSource->readyToSend = true;
+
+
+
+
+    // Wait forever
+    bool exit = false;
+    while (!exit)
+        programWaitMsg();
+
+    return 0;
+}
