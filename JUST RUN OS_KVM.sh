0687f2be2f0db1ec9433a8b0e7d7e21a1cfe722b
marceldobehere <marcel.z2006@gmail.com>
2025-12-21 01:45:40 +0100

    made the compilation work for newest version of fedora (mouse is borked sadly, will need to fix it somehow)

--- JUST RUN OS_KVM.sh
+++ JUST RUN OS_KVM.sh
@@ -2,5 +2,6 @@
 set -e
 
 ./cDisk.sh
-qemu-system-x86_64 -machine q35 -accel kvm -m 1G -cpu qemu64 -serial stdio -soundhw ac97 -drive file=disk.img -boot d -cdrom MaslOS2.iso
+qemu-system-x86_64 -machine q35 -accel kvm -m 1G -cpu qemu64 -serial stdio -drive file=disk.img -boot d -cdrom MaslOS2.iso
+# -soundhw ac97
 
